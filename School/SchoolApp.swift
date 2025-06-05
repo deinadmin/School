c822822ac5990aa2c7a9746cbe98ba7dcdfb0456
Carl Steen <carldodo@mail.de>
2025-06-05 18:05:07 +0200

    Initial Commit

---
+++ School/SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolApp.swift
+//  School
+//
+//  Created by Carl on 05.06.25.
+//
+
+import SwiftUI
+
+@main
+struct SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
